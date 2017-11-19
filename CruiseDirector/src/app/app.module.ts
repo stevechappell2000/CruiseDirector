c0c7dbf94f47f00ec4d0df7b4826f054e4b17a0c
steve <stevechappell2000@yahoo.com>
2017-11-19 05:47:25 -0800

    xx

--- CruiseDirector/src/app/app.module.ts
+++ CruiseDirector/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PluginsComponent } from './cruiseComponents/core/plugins/plugins.component';
+import { ProjectsComponent } from './cruiseComponents/core/projects/projects.component';
+import { PluginsService } from './cruiseComponents/core/dataservices/plugins.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PluginsComponent,
+    ProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PluginsService, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
